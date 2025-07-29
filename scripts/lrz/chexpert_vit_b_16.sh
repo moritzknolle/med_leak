8dbf64f556cf65a79afa225d67385bd1c79e4358
moritzknolle <moritz.knolle@me.com>
2025-07-29 16:37:02 +0200

    improved slurm scripts for LRZ

---
+++ scripts/lrz/chexpert_vit_b_16.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus-per-task=1
+#SBATCH --job-name=chexpert_vit_b_16
+#SBATCH --output=slurm_out/%j-%x-%t.out
+#SBATCH --mem-per-gpu=50g
+#SBATCH -p mcml-dgx-a100-40x8
+#SBATCH -q mcml
+#SBATCH -t 2-00:00:00
+
+source /dss/dsshome1/03/ga92hev2/miniconda3/etc/profile.d/conda.sh
+conda deactivate
+conda activate keras
+echo 'Opened virtual environment'
+
+srun python chexpert.py \
+    --eval_only=False \
+    --n_runs=200 \
+    --model='vit_b_16' \
+    --img_size=128,128  \
+    --save_root="/dss/dssmcmlfs01/pn67bo/pn67bo-dss-0000/moritz/npy" \
+    --ckpt_file_path="/dss/dssmcmlfs01/pn73mu/pn73mu-dss-0000/moritz/ckpts" \
+    --logdir="/dss/dssmcmlfs01/pn67bo/pn67bo-dss-0000/moritz/logs/chexpert/vit_b_16" \
---
+++ scripts/lrz/mimic-iv_tabresnet_300_6.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus-per-task=1
+#SBATCH --job-name=mimic-iv_tabresnet_300_6
+#SBATCH --output=slurm_out/%j-%x-%t.out
+#SBATCH --mem-per-gpu=100g
+#SBATCH -p mcml-hgx-a100-80x4-mig
+#SBATCH -q mcml
+#SBATCH -t 2-00:00:00
+
+source /dss/dsshome1/03/ga92hev2/miniconda3/etc/profile.d/conda.sh
+conda deactivate
+conda activate keras
+echo 'Opened virtual environment'
+
+srun python mimic-iv.py \
+    --eval_only=False \
+    --n_runs=200 \
+    --save_root="/dss/dssmcmlfs01/pn67bo/pn67bo-dss-0000/moritz/npy" \
+    --ckpt_file_path="/dss/dssmcmlfs01/pn73mu/pn73mu-dss-0000/moritz/ckpts" \
+    --logdir="/dss/dssmcmlfs01/pn67bo/pn67bo-dss-0000/moritz/logs/mimic-iv/resnet_300_6" \
