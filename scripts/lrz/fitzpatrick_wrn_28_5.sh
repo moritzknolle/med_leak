d42717af3590b1c805f2d40b9eea4760b5fee567
moritzknolle <moritz.knolle@me.com>
2025-07-31 16:05:55 +0200

    fix ckpt file path

--- scripts/lrz/fitzpatrick_wrn_28_5.sh
+++ scripts/lrz/fitzpatrick_wrn_28_5.sh
@@ -21,5 +21,5 @@ srun python fitzpatrick.py \
     --n_runs=200 \
     --model='wrn_28_5' \
     --save_root="/dss/dssmcmlfs01/pn67bo/pn67bo-dss-0000/moritz/npy" \
-    --ckpt_file_path="/dss/dssmcmlfs01/pn73mu/pn73mu-dss-0000/moritz/ckpts" \
+    --ckpt_file_path="/dss/dssmcmlfs01/pn67bo/pn67bo-dss-0000/moritz/ckpts" \
     --logdir="/dss/dssmcmlfs01/pn67bo/pn67bo-dss-0000/moritz/logs/fitzpatrick/wrn_28_5" \
