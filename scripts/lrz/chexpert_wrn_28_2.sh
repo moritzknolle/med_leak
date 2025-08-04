1d704696665dbcb883b9dd9a19029f90b811fa02
moritzknolle <moritz.knolle@me.com>
2025-08-04 17:41:46 +0200

    more bug fixes

--- scripts/lrz/chexpert_wrn_28_2.sh
+++ scripts/lrz/chexpert_wrn_28_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH --gres=gpu:4
-#SBATCH --ntasks=4
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks=8
 #SBATCH --cpus-per-task=8
 #SBATCH --gpus-per-task=1
 #SBATCH --job-name=chexpert_wrn_28_2
